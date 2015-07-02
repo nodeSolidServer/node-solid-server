a94c8e915a605fd1b11017beee82af6be3e2d2f0
Martin Martinez Rivera <martinmr@mit.edu>
2015-07-02 19:17:52 -0400

    Integrated with new version of node-webid

--- login.js
+++ login.js
@@ -19,14 +19,10 @@ function loginHandler(req, res, next) {
         var certificate = req.connection.getPeerCertificate();
         if (!_.isEmpty(certificate)) {
             var verifAgent = new webid.VerificationAgent(certificate);
-            verifAgent.verify(function(result) {
-                req.session.userId = result;
-                req.session.identified = true;
-                logging.log("Login -- Identified user: " + req.session.userId);
-                return next();
-            }, function(result) {
-                var message;
-                switch (result) {
+            verifAgent.verify(function(err, result) {
+                if (err) {
+                    var message;
+                    switch (err) {
                     case 'certificateProvidedSAN':
                         message = 'No valide Certificate Alternative Name in your certificate';
                         break;
@@ -42,10 +38,16 @@ function loginHandler(req, res, next) {
                     default:
                         message = "Unknown WebID error";
                         break;
+                    }
+                    logging.log("Login -- Error processing certificate: " + message);
+                    setEmptySession(req);
+                    return res.status(403).send(message);
+                } else {
+                    req.session.userId = result;
+                    req.session.identified = true;
+                    logging.log("Login -- Identified user: " + req.session.userId);
+                    return next();
                 }
-                logging.log("Login -- Error processing certificate: " + message);
-                setEmptySession(req);
-                return res.status(403).send(message);
             });
         } else {
             logging.log("Login -- Empty certificate");
