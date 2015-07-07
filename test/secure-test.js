37a69304ef9e76c585243e2b8833a28255cd51f0
Martin Martinez Rivera <martinmr@mit.edu>
2015-07-06 22:16:50 -0400

    Added ACL origin tests.

--- test/secure-test.js
+++ test/secure-test.js
@@ -13,6 +13,9 @@ var address = 'https://localhost:3456/test/';
 var aclDir = 'aclDir';
 var aclExtension = '.acl';
 var server = supertest(address);
+
+var user1 = "https://user1.databox.me/profile/card#me";
+var user2 = "https://user2.databox.me/profile/card#me";
 var userCredentials = {
     user1: {
         cert: fs.readFileSync('testfiles/user1-cert.pem'),
@@ -113,4 +116,66 @@ describe("Empty ACL Test", function() {
             done();
         });
     });
+});
+
+describe("ACL Origin Test", function() {
+    var origin1 = "http://example.org/";
+    var origin2 = "http://example.com/";
+    it("Should PUT new ACL file", function(done) {
+        var aclFile = aclDir + '/' + aclExtension;
+        var aclAddress = address + aclFile;
+        var options = createOptions(aclFile, 'user1');
+        options.headers = {
+            'content-type': 'text/turtle'
+        };
+        options.body = "<#Owner>\n" +
+            "	<http://www.w3.org/ns/auth/acl#accessTo> <" + address + aclDir + "/" + ">, <" + aclAddress + ">;\n" +
+            "	<http://www.w3.org/ns/auth/acl#agent> <" + user1 + ">;\n" +
+            "	<http://www.w3.org/ns/auth/acl#origin> <" + origin1 + ">;\n" +
+            "	<http://www.w3.org/ns/auth/acl#mode> <http://www.w3.org/ns/auth/acl#Read>, <http://www.w3.org/ns/auth/acl#Write> .\n" +
+            "<#Public>\n" +
+            "	<http://www.w3.org/ns/auth/acl#accessTo> <" + address + aclDir + "/" + ">;\n" +
+            "	<http://www.w3.org/ns/auth/acl#agentClass> <http://xmlns.com/foaf/0.1/Agent>;\n" +
+            "	<http://www.w3.org/ns/auth/acl#origin> <" + origin1 + ">;\n" +
+            "	<http://www.w3.org/ns/auth/acl#mode> <http://www.w3.org/ns/auth/acl#Read> .\n";
+        request.put(options, function(error, response, body) {
+            assert.equal(error, null);
+            assert.equal(response.statusCode, 201);
+            done();
+            //TODO triple header
+            //TODO user header
+        });
+    });
+    it("Should be able to access test directory", function(done) {
+        var options = createOptions(aclDir, 'user1');
+        request.head(options, function(error, response, body) {
+            assert.equal(error, null);
+            assert.equal(response.statusCode, 200);
+            done();
+        });
+    });
+    it("Should be able to access to test directory when origin is valid",
+       function(done) {
+           var options = createOptions(aclDir, 'user1');
+           options.headers = {
+               origin: origin1
+           };
+           request.head(options, function(error, response, body) {
+               assert.equal(error, null);
+               assert.equal(response.statusCode, 200);
+               done();
+           });
+       });
+    it("Should be denied access to test directory when origin is invalid",
+       function(done) {
+           var options = createOptions(aclDir, 'user1');
+           options.headers = {
+               origin: origin2
+           };
+           request.head(options, function(error, response, body) {
+               assert.equal(error, null);
+               assert.equal(response.statusCode, 403);
+               done();
+           });
+       });
 });
