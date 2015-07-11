a4d4fa4cc0ce85dd24a3ec5ae5699adc0b63dac4
nicola <notsecurity@gmail.com>
2015-07-11 17:20:37 +0200

    cleaning options.js

--- options.js
+++ options.js
@@ -10,21 +10,28 @@ module.exports = params;
 
 function params(argv) {
   var opts = {};
+
+  // From input
+  opts.cache = argv.cache;
+  opts.live = argv.live;
+  opts.path = opts.fileBase = argv.fileBase || process.cwd();
+  opts.port = argv.port;
+  opts.secret = argv.secret;
+  opts.ssl = argv.ssl;
+  // TODO no need of uriBase
+  opts.uri = opts.uriBase = argv.uriBase;
+  opts.verbose = argv.verbose;
+  opts.webid = argv.webid;
+
+  // Processed
   opts.leavePatchConnectionOpen = false;
   opts.aclSuffix = argv.aclSuffix || ",acl";
-  opts.uriBase = argv.uriBase ||
-      'http://localhost:3000' + process.cwd() + '/test/';
-  opts.fileBase = argv.fileBase || process.cwd();
+
   if (!(S(opts.fileBase).endsWith('/'))) {
       opts.fileBase += '/';
   }
-  opts.address = argv.a || '0.0.0.0';
-  opts.verbose = argv.v;
   opts.changesSuffix = argv.changesSuffix || ',changes';
   opts.SSESuffix = argv.SSESuffix || ',events';
-  opts.ssl = argv.ssl;
-  opts.cors = argv.cors;
-  debug("uriBase: " + opts.uriBase);
 
   opts.pathStart = '/' +
     opts.uriBase
@@ -38,18 +45,15 @@ function params(argv) {
     .slice(0, 3)
     .join('/');
 
-  debug("URI pathStart: " + opts.pathStart);
   opts.pathFilter = regexp().start(opts.pathStart).toRegExp();
   opts.xssProxy = argv.xssProxy;
   opts.proxyFilter = regexp().start(opts.xssProxy).toRegExp();
-  opts.live = argv.live;
-  opts.webid = argv.webid ? true : false;
-  opts.privateKey = argv.privateKey || path.join(opts.fileBase, 'key.pem');
-  opts.cert = argv.cert || path.join(opts.fileBase, 'cert.pem');
 
   // TODO this should be an attribute of an object
   opts.usedURIs = {};
 
+  debug("uriBase: " + opts.uriBase);
+  debug("pathStart: " + opts.pathStart);
   debug("URI path filter regexp: " + opts.pathFilter);
   debug("Verbose: " + !!opts.verbose);
   debug("Live: " + !!opts.live);
