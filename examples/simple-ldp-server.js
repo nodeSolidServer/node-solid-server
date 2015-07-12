45e3d09b0fa4c961d9e8701b6bf03e7179fcc82e
nicola <notsecurity@gmail.com>
2015-07-12 17:03:05 +0200

    simplifying handlers/put writeFile function

--- examples/simple-ldp-server.js
+++ examples/simple-ldp-server.js
@@ -6,13 +6,27 @@ var app = express()
 
 // My routes
 app.get('/', function (req, res) {
+  console.log(req)
   res.send('Welcome to my server!')
 })
 
 // Mounting ldnode on /ldp
-app.use('/ldp', ldnode({
+var ldp = ldnode({
   uri: 'http://localhost:3000/ldp'
-}))
+})
+var ldp1 = ldnode({
+  uri: 'http://localhost:3000/ldp/asd'
+})
+ldp.get('/jj', function (req, res) {
+  res.send(req.hostname + '\n' + req.baseUrl)
+  console.log(req.ip)
+})
+ldp1.get('/jj', function (req, res) {
+  res.send(req.hostname + '\n' + req.baseUrl)
+  console.log(req.ip)
+})
+ldp.use('/asd', ldp1)
+app.use('/ldp', ldp)
 
 // Starting server
 app.listen(3000, function () {
