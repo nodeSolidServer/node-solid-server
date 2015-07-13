0180ed816f03643aceb4e77cf968fd5704b99c56
nicola <notsecurity@gmail.com>
2015-07-14 00:05:33 +0200

    using testfiles dir instead

--- test/patch.js
+++ test/patch.js
@@ -21,14 +21,13 @@ function rm (file) {
 }
 
 function write (text, file) {
-  fsExtra.mkdirpSync(path.dirname(file));
   return fs.writeFileSync(__dirname + '/' + file, text);
 }
 
 
 describe('PATCH', function () {
   var ldp = ldnode.createServer({
-    base: __dirname + '/patchResources',
+    base: __dirname + '/testfiles',
     mount: '/test'
   });
   ldp.listen(3456);
@@ -37,7 +36,7 @@ describe('PATCH', function () {
   describe('POST', function() {
     write(
       '<#current> <#temp> 123 .',
-      'patchResources/emptyExample.ttl');
+      'testfiles/emptyExample.ttl');
 
     it('nothing should change with an empty file', function (done) {
       server.post('/emptyExample.ttl')
@@ -45,9 +44,9 @@ describe('PATCH', function () {
         .send('')
         .end(function(err, res, body){
           assert.equal(
-            read('patchResources/emptyExample.ttl'),
+            read('testfiles/emptyExample.ttl'),
             '\n   <#current> <#temp> 123 .\n');
-          rm('patchResources/emptyExample.ttl');
+          rm('testfiles/emptyExample.ttl');
           done(err);
         });
     });
