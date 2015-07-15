233fcccb560feb00e3b9635431605a8c51b97812
Nicola Greco <notsecurity@gmail.com>
2015-07-15 16:09:29 +0200

    removing unused code in `vocab/rdf.js`

--- vocab/rdf.js
+++ vocab/rdf.js
@@ -14,22 +14,6 @@
  * limitations under the License.
  */
 
-function define(name, value) {
-    Object.defineProperty(exports, name, {
-        value:      value,
-        enumerable: true
-    });
-}
-
-var ns = 'http://www.w3.org/1999/02/22-rdf-syntax-ns#';
-define('ns', ns);
-define('prefix', 'rdf');
-
-// Resources
-
-// Properties
-define('type', ns + 'type');
-
 function brack(s) {
     if (s.length > 0 && s[0] == "<") {
         return s;
