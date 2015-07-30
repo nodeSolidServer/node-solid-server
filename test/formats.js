bc3f130eab7f90bd8ec793a9d6584f4953c61c89
deiu <andrei@fcns.eu>
2015-07-30 18:37:26 -0400

    Added proper tests for conneg

--- test/formats.js
+++ test/formats.js
@@ -5,8 +5,6 @@ var fs = require('fs');
 var S = require('string');
 var ldnode = require('../index');
 
-var accepts = 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5';
-
 describe('formats', function () {
     var address = 'http://localhost:3457';
     var ldp = ldnode.createServer({
@@ -18,6 +16,7 @@ describe('formats', function () {
     describe('HTML', function() {
         it('Should return "Hello, World!"', function(done) {
             server.get('/hello.html')
+                .set('accept', 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5')
                 .expect('Content-type', /text\/html/)
                 .expect(/Hello, world!/)
                 .expect(200, done);
@@ -30,13 +29,13 @@ describe('formats', function () {
         };
         it('Should return JSON-LD document', function(done) {
             server.get('/patch-5-initial.ttl')
-                .set('accept', 'application/ld+json')
+                .set('accept', 'application/ld+json;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /application\/ld\+json/)
                 .expect(200, done);
         });
         it('Should return valid JSON', function(done) {
             server.get('/patch-5-initial.ttl')
-                .set('accept', 'application/ld+json')
+                .set('accept', 'application/ld+json;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect(isValidJSON)
                 .expect(200, done);
         });
@@ -45,7 +44,7 @@ describe('formats', function () {
     describe('N-Quads', function() {
         it('Should return N-Quads document', function(done) {
             server.get('/patch-5-initial.ttl')
-                .set('accept', 'application/n-quads')
+                .set('accept', 'application/n-quads;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /application\/n-quads/)
                 .expect(200, done);
         });
@@ -54,7 +53,7 @@ describe('formats', function () {
     describe('n3', function() {
         it('Should return turtle document if content-type set to n3', function(done) {
             server.get('/patch-5-initial.ttl')
-                .set('accept', 'text/n3')
+                .set('accept', 'text/n3;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /text\/n3/)
                 .expect(200, done);
         });
