eadf46ed96ecc836b8be24284a8362c075a9e4c8
deiu <andrei@fcns.eu>
2015-08-01 10:09:57 -0400

    Added an extra test, improved description for existing tests

--- test/formats.js
+++ test/formats.js
@@ -14,7 +14,7 @@ describe('formats', function () {
 
     var server = supertest(address);
     describe('HTML', function() {
-        it('Should return "Hello, World!"', function(done) {
+        it('Should return HTML containing "Hello, World!" if Accept is set to text/html', function(done) {
             server.get('/hello.html')
                 .set('accept', 'application/xml,application/xhtml+xml,text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5')
                 .expect('Content-type', /text\/html/)
@@ -27,13 +27,13 @@ describe('formats', function () {
         var isValidJSON = function(res) {
             var json = JSON.parse(res.text);
         };
-        it('Should return JSON-LD document', function(done) {
+        it('Should return JSON-LD document if Accept is set to application/ld+json', function(done) {
             server.get('/patch-5-initial.ttl')
                 .set('accept', 'application/ld+json;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /application\/ld\+json/)
                 .expect(200, done);
         });
-        it('Should return valid JSON', function(done) {
+        it('Should return valid JSON if Accept is set to JSON-LD', function(done) {
             server.get('/patch-5-initial.ttl')
                 .set('accept', 'application/ld+json;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect(isValidJSON)
@@ -42,7 +42,7 @@ describe('formats', function () {
     });
 
     describe('N-Quads', function() {
-        it('Should return N-Quads document', function(done) {
+        it('Should return N-Quads document is Accept is set to application/n-quads', function(done) {
             server.get('/patch-5-initial.ttl')
                 .set('accept', 'application/n-quads;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /application\/n-quads/)
@@ -51,10 +51,18 @@ describe('formats', function () {
     });
 
     describe('n3', function() {
-        it('Should return turtle document if content-type set to n3', function(done) {
+        it('Should return turtle document if Accept is set to text/n3', function(done) {
             server.get('/patch-5-initial.ttl')
                 .set('accept', 'text/n3;q=0.9,text/turtle;q=0.8,text/plain;q=0.7,*/*;q=0.5')
                 .expect('content-type', /text\/n3/)
+                .expect(200, done);
+        });
+    });
+
+    describe('none', function() {
+        it('Should return turtle document if no Accept header is set', function(done) {
+            server.get('/patch-5-initial.ttl')
+                .expect('content-type', /text\/turtle/)
                 .expect(200, done);
         });
     });
