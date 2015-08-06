333d46b2bed9ee3b7ba16030d8bd77ab253d1909
deiu <andrei@fcns.eu>
2015-08-06 10:38:15 -0400

    Added extra checks for basename()

--- test/utils.js
+++ test/utils.js
@@ -5,14 +5,20 @@ var utils = require('../lib/utils');
 describe('Utility functions', function() {
 
   describe('basename', function() {
-    it('should return bar as relative path', function() {
+    it('should return bar as relative path for /foo/bar', function() {
       assert.equal(utils.basename('/foo/bar'), 'bar');
     });
-    it('should return empty as relative path', function() {
+    it('should return empty as relative path for /foo/', function() {
       assert.equal(utils.basename('/foo/'), '');
     });
-    it('should return empty as relative path', function() {
+    it('should return empty as relative path for /', function() {
       assert.equal(utils.basename('/'), '');
+    });
+    it('should return empty as relative path for empty path', function() {
+      assert.equal(utils.basename(''), '');
+    });
+    it('should return empty as relative path for undefined path', function() {
+      assert.equal(utils.basename(undefined), '');
     });
   });
 });
