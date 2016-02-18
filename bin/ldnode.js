b9bdd11cc1c7a3ffa15bccfecef8dd6877f9980e
nicola <me@nicola.io>
2016-02-18 01:26:01 -0500

    final reordering

--- bin/ldnode.js
+++ bin/ldnode.js
@@ -4,38 +4,43 @@ var fs = require('fs')
 var path = require('path')
 var argv = require('nomnom')
   .script('ldnode')
+  .option('version', {
+    flag: true,
+    help: 'Print current ldnode version',
+    callback: function () {
+      fs.readFile(path.resolve(__dirname, '../package.json'), 'utf-8', function (_, file) {
+        console.log(JSON.parse(file).version)
+      })
+    }
+  })
+  .option('verbose', {
+    abbr: 'v',
+    flag: true,
+    help: 'Print the logs to console\n'
+  })
   .option('root', {
-    abbr: 'r',
     help: 'Root folder to serve (defaut: \'./\')'
   })
   .option('port', {
-    abbr: 'p',
     help: 'Port to use'
-  })
-  .option('key', {
-    help: 'Path to the SSL private key in PEM format',
-    abbr: 'K',
-    full: 'key'
-  })
-  .option('cert', {
-    full: 'cert',
-    help: 'Path to the SSL certificate key in PEM format',
-    abbr: 'C'
   })
   .option('webid', {
     help: 'Enable WebID+TLS authentication',
     full: 'webid',
     flag: true
   })
-  .option('idp', {
-    help: 'Allow users to register their WebID on subdomains',
-    abbr: 'idp',
-    full: 'identity-provider',
-    flag: true
+  .option('key', {
+    help: 'Path to the SSL private key in PEM format',
+    full: 'ssl-key'
   })
-  .option('secret', {
-    help: 'Secret used to sign the session ID cookie (e.g. "your secret phrase")',
-    abbr: 's'
+  .option('cert', {
+    full: 'ssl-cert',
+    help: 'Path to the SSL certificate key in PEM format'
+  })
+  .option('idp', {
+    help: 'Allow users to register their WebID on subdomains\n',
+    full: 'allow-signup',
+    flag: true
   })
   .option('createAdmin', {
     full: 'create-admin',
@@ -47,20 +52,25 @@ var argv = require('nomnom')
     help: 'Disable live support through WebSockets',
     flag: true
   })
+  .option('defaultApp', {
+    full: 'default-app',
+    help: 'URI to use as a default app for resources (default: https://linkeddata.github.io/warp/#/list/)'
+  })
   .option('proxy', {
     full: 'proxy',
-    help: 'Use a proxy on example.tld/proxyPath',
-    abbr: 'P'
+    help: 'Use a proxy on example.tld/proxyPath'
   })
   .option('suffixAcl', {
     full: 'suffix-acl',
-    help: "Suffix for acl files (default: '.acl')",
-    abbr: 'sA'
+    help: 'Suffix for acl files (default: \'.acl\')'
   })
   .option('suffixMeta', {
     full: 'suffix-meta',
-    help: "Suffix for metadata files (default: '.meta')",
-    abbr: 'sM'
+    help: 'Suffix for metadata files (default: \'.meta\')'
+  })
+  .option('secret', {
+    help: 'Secret used to sign the session ID cookie (e.g. "your secret phrase")',
+    full: 'session-secret'
   })
   .option('noErrorPages', {
     full: 'no-error-pages',
@@ -71,32 +81,12 @@ var argv = require('nomnom')
     full: 'error-pages',
     help: 'Folder from which to look for custom error pages files (files must be named <error-code>.html -- eg. 500.html)'
   })
-  .option('defaultApp', {
-    full: 'default-app',
-    help: 'URI to use as a default app for resources (default: https://linkeddata.github.io/warp/#/list/)'
-  })
   .option('mount', {
-    abbr: 'm',
     help: 'Serve on a specific URL path (default: \'/\')'
   })
   .option('forceUser', {
     help: 'Force a WebID to always be logged in (useful when offline)',
-    abbr: 'fU',
     full: 'force-user'
-  })
-  .option('verbose', {
-    abbr: 'v',
-    flag: true,
-    help: 'Print the logs to console'
-  })
-  .option('version', {
-    flag: true,
-    help: 'Print current ldnode version',
-    callback: function () {
-      fs.readFile(path.resolve(__dirname, '../package.json'), 'utf-8', function (_, file) {
-        console.log(JSON.parse(file).version)
-      })
-    }
   })
   .parse()
 
