27bfdfebad52c81ee073e82feeddb4a9b041ec73
nicola <me@nicola.io>
2016-05-12 12:59:29 -0400

    do not skip the tests

--- test/api-accounts.js
+++ test/api-accounts.js
@@ -67,19 +67,19 @@ describe('OIDC flow', () => {
     if (bobServer) bobServer.close()
   })
 
-  it.skip('step1: User tries to get /foo and gets 401 and meta redirect', (done) => {
+  it('step1: User tries to get /foo and gets 401 and meta redirect', (done) => {
     getBobFoo(alice, bob, done)
   })
 
-  it.skip('step2: User enters webId to signin', (done) => {
+  it('step2: User enters webId to signin', (done) => {
     postBobDiscoverSignIn(alice, bob, done)
   })
 
-  it.skip('step3: User enters password', (done) => {
+  it('step3: User enters password', (done) => {
     entersPasswordAndConsent(alice, bob, done)
   })
 
-  it.skip('entire flow', (done) => {
+  it('entire flow', (done) => {
     waterfall([
       (cb) => getBobFoo(alice, bob, cb),
       (cb) => postBobDiscoverSignIn(alice, bob, cb),
