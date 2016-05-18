26cf4c6dd60e842301397b84e28b07101d215732
nicola <me@nicola.io>
2016-05-17 20:13:38 -0400

    indentation issue

--- test/capability-discovery.js
+++ test/capability-discovery.js
@@ -32,53 +32,53 @@ describe('API', () => {
     if (aliceServer) aliceServer.close()
   })
 
-describe('Capability Discovery', function () {
-  describe('GET Service Capability document', function () {
-    it('should exist', function (done) {
-      alice.get('/.well-known/solid')
-        .expect(200, done)
-    })
-    it('should be a json file by default', function (done) {
-      alice.get('/.well-known/solid')
-        .expect('content-type', /application\/json/)
-        .expect(200, done)
-    })
-    it('includes a root element', function (done) {
-      alice.get('/.well-known/solid')
-        .end(function (err, req) {
-          expect(req.body.root).to.exist
-          return done(err)
-        })
-    })
-    it('includes an apps config section', function (done) {
-      const config = {
-        apps: {
-          'signin': '/signin/',
-          'signup': '/signup/'
+  describe('Capability Discovery', function () {
+    describe('GET Service Capability document', function () {
+      it('should exist', function (done) {
+        alice.get('/.well-known/solid')
+          .expect(200, done)
+      })
+      it('should be a json file by default', function (done) {
+        alice.get('/.well-known/solid')
+          .expect('content-type', /application\/json/)
+          .expect(200, done)
+      })
+      it('includes a root element', function (done) {
+        alice.get('/.well-known/solid')
+          .end(function (err, req) {
+            expect(req.body.root).to.exist
+            return done(err)
+          })
+      })
+      it('includes an apps config section', function (done) {
+        const config = {
+          apps: {
+            'signin': '/signin/',
+            'signup': '/signup/'
+          }
         }
-      }
-      const solid = Solid(config)
-      let server = supertest(solid)
-      server.get('/.well-known/solid')
-        .end(function (err, req) {
-          expect(req.body.apps).to.exist
-          return done(err)
-        })
+        const solid = Solid(config)
+        let server = supertest(solid)
+        server.get('/.well-known/solid')
+          .end(function (err, req) {
+            expect(req.body.apps).to.exist
+            return done(err)
+          })
+      })
     })
-  })
 
-  describe('OPTIONS API', function () {
-    it('should set the service Link header', function (done) {
-      alice.options('/')
-        .expect('Link', /<.*\.well-known\/solid>; rel="service"/)
-        .expect(204, done)
-    })
-    it('should still have previous link headers', function (done) {
-      alice.options('/')
-        .expect('Link', /<http:\/\/www.w3.org\/ns\/ldp#BasicContainer>; rel="type"/)
-        .expect('Link', /<http:\/\/www.w3.org\/ns\/ldp#Container>; rel="type"/)
-        .expect(204, done)
+    describe('OPTIONS API', function () {
+      it('should set the service Link header', function (done) {
+        alice.options('/')
+          .expect('Link', /<.*\.well-known\/solid>; rel="service"/)
+          .expect(204, done)
+      })
+      it('should still have previous link headers', function (done) {
+        alice.options('/')
+          .expect('Link', /<http:\/\/www.w3.org\/ns\/ldp#BasicContainer>; rel="type"/)
+          .expect('Link', /<http:\/\/www.w3.org\/ns\/ldp#Container>; rel="type"/)
+          .expect(204, done)
+      })
     })
   })
-})
 })
