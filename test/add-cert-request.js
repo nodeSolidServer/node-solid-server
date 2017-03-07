9f7887e2a185277a27e0d36bdc41264875cd24c8
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-03-07 16:32:40 -0500

    Add generateCertificate() test

--- test/add-cert-request.js
+++ test/add-cert-request.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const fs = require('fs-extra')
+const path = require('path')
+const rdf = require('rdflib')
+const ns = require('solid-namespace')(rdf)
 const chai = require('chai')
 const expect = chai.expect
 const sinon = require('sinon')
@@ -8,10 +12,14 @@ chai.use(sinonChai)
 chai.should()
 const HttpMocks = require('node-mocks-http')
 
-// const LDP = require('../lib/ldp')
 const SolidHost = require('../lib/models/solid-host')
 const AccountManager = require('../lib/models/account-manager')
 const AddCertificateRequest = require('../lib/requests/add-cert-request')
+const WebIdTlsCertificate = require('../lib/models/webid-tls-certificate')
+
+const exampleSpkac = fs.readFileSync(
+  path.join(__dirname, './resources/example_spkac.cnf'), 'utf8'
+)
 
 var host
 
@@ -65,6 +73,43 @@ describe('AddCertificateRequest', () => {
       return AddCertificateRequest.addCertificate(request)
         .then(() => {
           expect(certSpy).to.have.been.called
+        })
+    })
+  })
+
+  describe('accountManager.addCertKeyToGraph()', () => {
+    let multiUser = true
+
+    it('should add certificate data to a graph', () => {
+      let options = { host, multiUser, authMethod: 'tls' }
+      let accountManager = AccountManager.from(options)
+
+      let userData = { username: 'alice' }
+      let userAccount = accountManager.userAccountFrom(userData)
+
+      let certificate = WebIdTlsCertificate.fromSpkacPost(
+        decodeURIComponent(exampleSpkac),
+        userAccount,
+        host)
+
+      let graph = rdf.graph()
+
+      return certificate.generateCertificate()
+        .then(() => {
+          return accountManager.addCertKeyToGraph(certificate, graph)
+        })
+        .then(graph => {
+          let webId = rdf.namedNode(certificate.webId)
+          let key = rdf.namedNode(certificate.keyUri)
+
+          expect(graph.anyStatementMatching(webId, ns.cert('key'), key))
+            .to.exist
+          expect(graph.anyStatementMatching(key, ns.rdf('type'), ns.cert('RSAPublicKey')))
+            .to.exist
+          expect(graph.anyStatementMatching(key, ns.cert('modulus')))
+            .to.exist
+          expect(graph.anyStatementMatching(key, ns.cert('exponent')))
+            .to.exist
         })
     })
   })
