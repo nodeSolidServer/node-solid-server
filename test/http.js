898339463a2305bb56f0c9db705325d079acce46
Dmitri Zagidulin <dzagidulin@gmail.com>
2017-06-01 13:25:40 -0400

    Update glob test / expected value

--- test/http.js
+++ test/http.js
@@ -286,23 +286,24 @@ describe('HTTP APIs', function () {
         .expect(200, done)
     })
     it('should have glob support', function (done) {
-      let expectedValue = `@prefix : <#>.
-@prefix ex: <example1.ttl#>.
-@prefix n0: <http://example.org/stuff/1.0/>.
+      let expectedValue = `@prefix dc: <http://purl.org/dc/elements/1.1/>.
+@prefix ex: <http://example.org/stuff/1.0/>.
+@prefix e: <example1.ttl#>.
 @prefix TR: <http://www.w3.org/TR/>.
 @prefix d: <http://purl.org/net/dajobe/>.
-@prefix n1: <http://purl.org/dc/elements/1.1/>.
 
-<> a ex:glob.
-
-n0:a
-    n0:b
+   e:this dc:title "Test title" .
+TR:rdf-syntax-grammar
+    ex:editor
+       [ ex:fullname "Dave Beckett"; ex:homePage d: ];
+    dc:title
+       "RDF/XML Syntax Specification (Revised)".
+ex:a
+    ex:b
         """The first line
 The second line
-  more""", ( "apple" "banana" ).
-TR:rdf-syntax-grammar
-    n0:editor [ n0:fullname "Dave Beckett"; n0:homePage d: ];
-    n1:title "RDF/XML Syntax Specification (Revised)".
+  more""",
+           ( "apple" "banana" ).
 `
       server.get('/sampleContainer/example*')
         .expect('content-type', /text\/turtle/)
