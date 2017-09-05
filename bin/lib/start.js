68395283670b493910b8a494ac0ae71cae9e3821
Ruben Verborgh <ruben@verborgh.org>
2017-09-05 14:35:47 -0400

    Display git commit info when available.
    Closes #580.

--- bin/lib/start.js
+++ bin/lib/start.js
@@ -3,7 +3,6 @@
 const options = require('./options')
 const fs = require('fs')
 const extend = require('extend')
-const packageJson = require('../../package.json')
 const colors = require('colors/safe')
 
 module.exports = function (program, server) {
@@ -24,7 +23,7 @@ module.exports = function (program, server) {
   start.option('-v, --verbose', 'Print the logs to console')
 
   start.action((opts) => {
-    let argv = extend({}, opts)
+    let argv = extend({}, opts, { version: program.version() })
 
     fs.readFile(process.cwd() + '/config.json', (err, file) => {
       // No file exists, not a problem
@@ -131,7 +130,7 @@ function bin (argv, server) {
     return 1
   }
   app.listen(argv.port, function () {
-    console.log('Solid server (solid v' + packageJson.version + ') running on \u001b[4mhttps://localhost:' + argv.port + '/\u001b[0m')
+    console.log(`Solid server (${argv.version}) running on \u001b[4mhttps://localhost:${argv.port}/\u001b[0m`)
     console.log('Press <ctrl>+c to stop')
   })
 }
