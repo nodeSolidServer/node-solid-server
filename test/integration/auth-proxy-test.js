d9355b84704f655a587691fc9fc07884750994ae
Ruben Verborgh <ruben@verborgh.org>
2017-09-15 15:00:52 -0400

    Require write permissions for unsafe proxy methods.
    Closes #595.

--- test/integration/auth-proxy-test.js
+++ test/integration/auth-proxy-test.js
@@ -5,7 +5,6 @@ const request = require('supertest')
 const { expect } = require('chai')
 const rm = require('../utils').rm
 
-const HOST = 'solid.org'
 const USER = 'https://ruben.verborgh.org/profile/#me'
 
 describe('Auth Proxy', () => {
@@ -15,6 +14,8 @@ describe('Auth Proxy', () => {
       // Set up test back-end server
       nock('http://server-a.org').persist()
         .get(/./).reply(200, function () { return this.req.headers })
+        .options(/./).reply(200)
+        .post(/./).reply(200)
 
       // Set up Solid server
       server = ldnode({
@@ -36,18 +37,100 @@ describe('Auth Proxy', () => {
 
     describe('responding to /server/a', () => {
       let response
-      before(() => {
-        return request(server).get('/server/a')
-          .set('Host', HOST)
+      before(() =>
+        request(server).get('/server/a/')
           .then(res => { response = res })
-      })
+      )
 
       it('sets the User header on the proxy request', () => {
         expect(response.body).to.have.property('user', USER)
       })
+    })
 
-      it('returns status code 200', () => {
-        expect(response).to.have.property('statusCode', 200)
+    describe('responding to GET', () => {
+      describe('for a path with read permissions', () => {
+        let response
+        before(() =>
+          request(server).get('/server/a/r')
+            .then(res => { response = res })
+        )
+        it('returns status code 200', () => {
+          expect(response).to.have.property('statusCode', 200)
+        })
+      })
+
+      describe('for a path without read permissions', () => {
+        let response
+        before(() =>
+          request(server).get('/server/a/wc')
+            .then(res => { response = res })
+        )
+
+        it('returns status code 403', () => {
+          expect(response).to.have.property('statusCode', 403)
+        })
+      })
+    })
+
+    describe('responding to OPTIONS', () => {
+      describe('for a path with read permissions', () => {
+        let response
+        before(() =>
+          request(server).options('/server/a/r')
+            .then(res => { response = res })
+        )
+        it('returns status code 200', () => {
+          expect(response).to.have.property('statusCode', 200)
+        })
+      })
+
+      describe('for a path without read permissions', () => {
+        let response
+        before(() =>
+          request(server).options('/server/a/wc')
+            .then(res => { response = res })
+        )
+
+        it('returns status code 403', () => {
+          expect(response).to.have.property('statusCode', 403)
+        })
+      })
+    })
+
+    describe('responding to POST', () => {
+      describe('for a path with read and write permissions', () => {
+        let response
+        before(() =>
+          request(server).post('/server/a/rw')
+            .then(res => { response = res })
+        )
+        it('returns status code 200', () => {
+          expect(response).to.have.property('statusCode', 200)
+        })
+      })
+
+      describe('for a path without read permissions', () => {
+        let response
+        before(() =>
+          request(server).post('/server/a/w')
+            .then(res => { response = res })
+        )
+
+        it('returns status code 403', () => {
+          expect(response).to.have.property('statusCode', 403)
+        })
+      })
+
+      describe('for a path without write permissions', () => {
+        let response
+        before(() =>
+          request(server).post('/server/a/r')
+            .then(res => { response = res })
+        )
+
+        it('returns status code 403', () => {
+          expect(response).to.have.property('statusCode', 403)
+        })
       })
     })
   })
