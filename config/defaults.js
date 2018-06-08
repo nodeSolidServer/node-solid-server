c7bd4a5308168b77643cafc461d5d5eec88a9fdf
Sergey Malinin <sergmalinin@gmail.com>
2018-06-08 19:45:22 +1000

    Sync with master ver 4.1.0

--- config/defaults.js
+++ config/defaults.js
@@ -12,4 +12,12 @@ module.exports = {
   'serverUri': 'https://localhost:8443',
   'webid': true,
   'dataBrowserPath': 'default'
+
+  // For use in Enterprises to configure a HTTP proxy for all outbound HTTP requests from the SOLID server (we use
+  // https://www.npmjs.com/package/global-tunnel-ng).
+  // "httpProxy": {
+  //   "tunnel": "neither",
+  //   "host": "proxy.example.com",
+  //   "port": 12345
+  // }
 }
