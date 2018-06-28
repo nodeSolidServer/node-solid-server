2a0ca116efa8fd470a169b0f30827376e8108c0c
Dmitri Zagidulin <dzagidulin@gmail.com>
2018-06-28 10:01:20 -0400

    Pass in fetchGraph to ACLChecker/PermissionSet

--- test/unit/acl-checker-test.js
+++ test/unit/acl-checker-test.js
@@ -4,21 +4,26 @@ const chai = require('chai')
 const { expect } = chai
 chai.use(require('chai-as-promised'))
 
+const options = { fetch: (url, callback) => {} }
+
 describe('ACLChecker unit test', () => {
+  let acl
+
+  beforeEach(() => {
+    acl = new ACLChecker('http://ex.com/.acl', options)
+  })
+
   describe('checkAccess', () => {
     it('should callback with null on grant success', () => {
-      let acl = new ACLChecker()
       let acls = { checkAccess: () => Promise.resolve(true) }
       return expect(acl.checkAccess(acls)).to.eventually.be.true
     })
     it('should callback with error on grant failure', () => {
-      let acl = new ACLChecker()
       let acls = { checkAccess: () => Promise.resolve(false) }
       return expect(acl.checkAccess(acls))
       .to.be.rejectedWith('ACL file found but no matching policy found')
     })
     it('should callback with error on grant error', () => {
-      let acl = new ACLChecker()
       let acls = { checkAccess: () => Promise.reject(new Error('my error')) }
       return expect(acl.checkAccess(acls)).to.be.rejectedWith('my error')
     })
@@ -26,14 +31,14 @@ describe('ACLChecker unit test', () => {
 
   describe('getPossibleACLs', () => {
     it('returns all possible ACLs of the root', () => {
-      const aclChecker = new ACLChecker('http://ex.org/')
+      const aclChecker = new ACLChecker('http://ex.org/', options)
       expect(aclChecker.getPossibleACLs()).to.deep.equal([
         'http://ex.org/.acl'
       ])
     })
 
     it('returns all possible ACLs of a regular file', () => {
-      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi')
+      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi', options)
       expect(aclChecker.getPossibleACLs()).to.deep.equal([
         'http://ex.org/abc/def/ghi.acl',
         'http://ex.org/abc/def/.acl',
@@ -43,7 +48,7 @@ describe('ACLChecker unit test', () => {
     })
 
     it('returns all possible ACLs of an ACL file', () => {
-      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi.acl')
+      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi.acl', options)
       expect(aclChecker.getPossibleACLs()).to.deep.equal([
         'http://ex.org/abc/def/ghi.acl',
         'http://ex.org/abc/def/.acl',
@@ -53,7 +58,7 @@ describe('ACLChecker unit test', () => {
     })
 
     it('returns all possible ACLs of a directory', () => {
-      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi/')
+      const aclChecker = new ACLChecker('http://ex.org/abc/def/ghi/', options)
       expect(aclChecker.getPossibleACLs()).to.deep.equal([
         'http://ex.org/abc/def/ghi/.acl',
         'http://ex.org/abc/def/.acl',
