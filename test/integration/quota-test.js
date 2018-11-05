790e8ea62069ed8b2d197025602bedda2b62f1fa
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2018-11-05 13:48:32 +0100

    Fix the tests to async, thanks @megoth

--- test/integration/quota-test.js
+++ test/integration/quota-test.js
@@ -12,25 +12,31 @@ describe('Get Quota', function () {
     expect(prefs).to.be.a('string')
     expect(prefs).to.match(/storageQuota/)
   })
-  it('Get the quota', function (done) {
-    expect(getQuota(path.join('test/resources/', root), 'https://localhost')).to.eventually.equal(2000)
+  it('Get the quota', async function () {
+    const quota = await getQuota(path.join('test/resources/', root), 'https://localhost')
+    expect(quota).to.equal(2000)
   })
-/*  it('Get the quota with non-existant file', function () {
-    expect(getQuota(path.join('nowhere/', root), 'https://localhost')).to.equal(Infinity)
+  it('Get the quota with non-existant file', async function () {
+    const quota = await getQuota(path.join('nowhere/', root), 'https://localhost')
+    expect(quota).to.equal(Infinity)
   })
-  it('Get the quota when the predicate is not present', function () {
-    expect(getQuota('test/resources/accounts-acl/quota', 'https://localhost')).to.equal(Infinity)
-  }) */
+  it('Get the quota when the predicate is not present', async function () {
+    const quota = await getQuota('test/resources/accounts-acl/quota', 'https://localhost')
+    expect(quota).to.equal(Infinity)
+  })
 })
 
 describe('Check if over Quota', function () {
-  it('Check the quota', function () {
-    expect(overQuota(path.join('test/resources/', root), 'https://localhost')).to.be.true
+  it('Check the quota', async function () {
+    const quota = await overQuota(path.join('test/resources/', root), 'https://localhost')
+    expect(quota).to.be.true
   })
-  it('Check the quota with non-existant file', function () {
-    expect(overQuota(path.join('nowhere/', root), 'https://localhost')).to.be.false
+  it('Check the quota with non-existant file', async function () {
+    const quota = await overQuota(path.join('nowhere/', root), 'https://localhost')
+    expect(quota).to.be.false
   })
-  it('Check the quota when the predicate is not present', function () {
-    expect(overQuota('test/resources/accounts-acl/quota', 'https://localhost')).to.be.false()
+  it('Check the quota when the predicate is not present', async function () {
+    const quota = await overQuota('test/resources/accounts-acl/quota', 'https://localhost')
+    expect(quota).to.be.false
   })
 })
