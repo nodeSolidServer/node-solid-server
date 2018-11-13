ebfe3f3c4946d3d366f8d736cda47768809bb35f
Arne Hassel <arne.hassel@gmail.com>
2018-11-13 13:17:12 +0100

    Just a minor commit

--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,10 @@
-var fs = require('fs-extra')
-var rimraf = require('rimraf')
-var path = require('path')
+const fs = require('fs-extra')
+const rimraf = require('rimraf')
+const path = require('path')
 const OIDCProvider = require('@solid/oidc-op')
 const dns = require('dns')
+const ldnode = require('../index')
+const supertest = require('supertest')
 
 const TEST_HOSTS = ['nic.localhost', 'tim.localhost', 'nicola.localhost']
 
@@ -79,4 +81,53 @@ exports.loadProvider = function loadProvider (configPath) {
 
       return provider.initializeKeyChain(config.keys)
     })
+}
+
+exports.createServer = createServer
+function createServer (options) {
+  return ldnode.createServer(options)
+}
+
+exports.setupSuperServer = setupSuperServer
+function setupSuperServer (options) {
+  const ldpServer = createServer(options)
+  return supertest(ldpServer)
+}
+
+/**
+ * Creates a new test basic container via an LDP POST
+ *   (located in `test/resources/{containerName}`)
+ * @method createTestContainer
+ * @param server {supertest} An instance of a supertest server
+ * @param containerName {String} Container name used as slug, no leading `/`
+ * @return {Promise} Promise obj, for use with Mocha's `before()` etc
+ */
+exports.createTestContainer = function createTestContainer (server, containerName) {
+  return new Promise(function (resolve, reject) {
+    server.post('/')
+      .set('content-type', 'text/turtle')
+      .set('slug', containerName)
+      .set('link', '<http://www.w3.org/ns/ldp#BasicContainer>; rel="type"')
+      .set('content-type', 'text/turtle')
+      .end(function (error, res) {
+        error ? reject(error) : resolve(res)
+      })
+  })
+}
+
+/**
+ * Creates a new turtle test resource via an LDP PUT
+ *   (located in `test/resources/{resourceName}`)
+ * @method createTestResource
+ * @param resourceName {String} Resource name (should have a leading `/`)
+ * @return {Promise} Promise obj, for use with Mocha's `before()` etc
+ */
+exports.createTestResource = function createTestResource (server, resourceName) {
+  return new Promise(function (resolve, reject) {
+    server.put(resourceName)
+      .set('content-type', 'text/turtle')
+      .end(function (error, res) {
+        error ? reject(error) : resolve(res)
+      })
+  })
 }
