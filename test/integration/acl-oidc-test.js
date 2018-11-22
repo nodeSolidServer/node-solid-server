d0666f7a9bacfc7b8a0ddb4f29b6ee8097128b50
Kjetil Kjernsmo <kjetil@kjernsmo.net>
2018-11-22 16:23:49 +0100

    Note TODO status

--- test/integration/acl-oidc-test.js
+++ test/integration/acl-oidc-test.js
@@ -169,7 +169,7 @@ describe('ACL with WebID+OIDC over HTTP', function () {
         options.body = ''
         request.put(options, function (error, response, body) {
           assert.equal(error, null)
-          assert.equal(response.statusCode, 409)
+          assert.equal(response.statusCode, 409) // TODO
           done()
         })
       })
@@ -307,7 +307,7 @@ describe('ACL with WebID+OIDC over HTTP', function () {
 
         request.head(options, function (error, response, body) {
           assert.equal(error, null)
-          assert.equal(response.statusCode, 403)
+          assert.equal(response.statusCode, 403) // TODO
           assert.equal(response.statusMessage, 'Origin Unauthorized')
           done()
         })
