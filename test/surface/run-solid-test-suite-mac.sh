871d0cf7373b0487e9a462f931aca1ed7f19ea53
Michiel de Jong <michiel@unhosted.org>
2020-10-02 13:07:48 +0200

    Oops

--- test/surface/run-solid-test-suite-mac.sh
+++ test/surface/run-solid-test-suite-mac.sh
@@ -2,10 +2,10 @@
 set -e
 
 docker network create testnet
+docker build -t node-solid-server https://github.com/solid/test-suite.git#master:/servers/node-solid-server
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
-docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
+# docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
 # docker build -t web-access-control https://github.com/michielbdejong/test-suite.git#add-testers:/testers/web-access-control
-# docker build -t node-solid-server https://github.com/solid/test-suite.git#master:/servers/node-solid-server
 docker run --rm -d --name server --network=testnet -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
 docker logs server
--- test/surface/run-solid-test-suite-travis.sh
+++ test/surface/run-solid-test-suite-travis.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
+docker build -t node-solid-server https://github.com/solid/test-suite.git#master:/servers/node-solid-server
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
-docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
 docker run --rm -d --name server --network=host -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
 docker logs server
 docker ps -a
