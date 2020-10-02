a4339aae0040f36d8c004fafd6be49d571453765
Michiel de Jong <michiel@unhosted.org>
2020-10-02 11:09:23 +0200

    Fix git urls and mount in current code

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -2,12 +2,11 @@
 set -e
 
 docker network create testnet
-docker build -t webid-provider https://github.com/solid/test-suite#:/testers/webid-provider
-docker build -t node-solid-server https://github.com/solid/test-suite#:/servers/node-solid-server
-docker run --rm -d --name=server --network=testnet node-solid-server
+docker build -t webid-provider https://github.com/solid/test-suite.git#:/testers/webid-provider
+docker build -t node-solid-server https://github.com/solid/test-suite.git#:/servers/node-solid-server
+docker run --rm -d --name=server --network=testnet -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
-# docker run --rm --network=testnet --name tester --env-file /tmp/env-vars-for-test-image.list webid-provider
-docker run --rm --network=testnet --name tester webid-provider
+docker run --rm --network=testnet --name tester --env-file /tmp/env-vars-for-test-image.list webid-provider
 rm /tmp/env-vars-for-test-image.list
 docker stop server
 docker network remove testnet
