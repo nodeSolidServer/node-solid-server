c43d4d9f9aad7eabce029b53d6ed08f8fec90ab2
Michiel de Jong <michiel@unhosted.org>
2020-10-02 10:48:23 +0200

    make tests fail

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -5,7 +5,8 @@ docker build -t webid-provider https://github.com/solid/test-suite#:/testers/web
 docker build -t node-solid-server https://github.com/solid/test-suite#:/servers/node-solid-server
 docker run --rm -d --name=server --network=testnet node-solid-server
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
-docker run --rm --network=testnet --name tester --env-file /tmp/env-vars-for-test-image.list webid-provider
+# docker run --rm --network=testnet --name tester --env-file /tmp/env-vars-for-test-image.list webid-provider
+docker run --rm --network=testnet --name tester webid-provider
 rm /tmp/env-vars-for-test-image.list
 docker stop server
 docker network remove testnet
