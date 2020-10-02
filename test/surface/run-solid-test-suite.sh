a46ab67ba5c93027ca9a7742d0be95fcd3f5f362
Michiel de Jong <michiel@unhosted.org>
2020-10-02 14:18:01 +0200

    Leave server after it exits

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -6,7 +6,7 @@ docker build -t node-solid-server https://github.com/solid/test-suite.git#master
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
 # docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
 # docker build -t web-access-control https://github.com/michielbdejong/test-suite.git#add-testers:/testers/web-access-control
-docker run --rm -d --name server --network=testnet -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
+docker run -d --name server --network=testnet -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
 do
@@ -23,4 +23,5 @@ docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list w
 # docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list web-access-control
 rm /tmp/env-vars-for-test-image.list
 docker stop server
+docker rm server
 docker network remove testnet
