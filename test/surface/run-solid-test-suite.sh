beb4d3c2f1dd6f9dc8f72a289bcbe604d084773e
Michiel de Jong <michiel@unhosted.org>
2020-10-02 14:36:25 +0200

    run in /node-solid-server

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -6,7 +6,7 @@ docker build -t node-solid-server https://github.com/solid/test-suite.git#master
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
 # docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
 # docker build -t web-access-control https://github.com/michielbdejong/test-suite.git#add-testers:/testers/web-access-control
-docker run -d --name server --network=testnet -v `pwd`:/travis -w /travis node-solid-server ./bin/solid-test start --config-file /node-solid-server/config.json
+docker run -d --name server --network=testnet -v `pwd`:/travis -w /node-solid-server node-solid-server /travis/bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
 until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
 do
