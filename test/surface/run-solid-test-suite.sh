59e1453782a193decdd847c0ae782fbcad6e30f6
Michiel de Jong <michiel@unhosted.org>
2020-10-19 14:42:24 +0200

    uncomment solid-crud tests

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -4,7 +4,7 @@ set -e
 docker network create testnet
 docker build -t node-solid-server https://github.com/solid/test-suite.git#master:/servers/node-solid-server
 docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
-# docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
+docker build -t solid-crud https://github.com/michielbdejong/test-suite.git#add-testers:/testers/solid-crud
 # docker build -t web-access-control https://github.com/michielbdejong/test-suite.git#add-testers:/testers/web-access-control
 docker run -d --name server --network=testnet -v `pwd`:/travis -w /node-solid-server node-solid-server /travis/bin/solid-test start --config-file /node-solid-server/config.json
 wget -O /tmp/env-vars-for-test-image.list https://raw.githubusercontent.com/solid/test-suite/master/servers/node-solid-server/env.list
@@ -19,7 +19,7 @@ done
 docker ps -a
 docker logs server
 docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list webid-provider
-# docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list solid-crud
+docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list solid-crud
 # docker run --rm --network=testnet --env-file /tmp/env-vars-for-test-image.list web-access-control
 rm /tmp/env-vars-for-test-image.list
 docker stop server
