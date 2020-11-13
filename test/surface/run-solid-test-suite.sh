b622afd18e6d61a0548a4516c37b64d3cfeaed9b
Michiel de Jong <michiel@unhosted.org>
2020-11-13 10:35:09 +0100

    Use local docker images

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -4,9 +4,11 @@ set -e
 docker network create testnet
 docker build -t server test/surface/docker/server
 docker build -t cookie test/surface/docker/cookie
-docker build -t webid-provider https://github.com/solid/test-suite.git#master:/testers/webid-provider
-docker build -t solid-crud https://github.com/solid/test-suite.git#master:/testers/solid-crud
-docker build -t web-access-control https://github.com/solid/test-suite.git#master:/testers/web-access-control
+
+docker build -t webid-provider test/surface/docker/webid-provider
+docker build -t solid-crud test/surface/docker/solid-crud
+docker build -t web-access-control test/surface/docker/web-access-control
+
 docker run -d --env-file test/surface/server-env.list --name server --network=testnet -v `pwd`:/travis -w /node-solid-server server /travis/bin/solid-test start --config-file /node-solid-server/config.json
 docker run -d --env-file test/surface/thirdparty-env.list --name thirdparty --network=testnet -v `pwd`/test/surface:/surface server /node-solid-server/bin/solid-test start --config-file /surface/thirdparty-config.json
 
@@ -28,11 +30,12 @@ done
 
 docker ps -a
 docker logs server
+
 export COOKIE_ALICE="`docker run --rm -e SERVER_ROOT="https://server" --network=testnet cookie`"
 export COOKIE_BOB="`docker run --rm -e SERVER_ROOT="https://thirdparty" --network=testnet cookie`"
 
 docker run --rm --network=testnet --env COOKIE="$COOKIE_ALICE" --env-file test/surface/webid-provider-tests-env.list webid-provider
-# docker run --rm --network=testnet --env COOKIE="$COOKIE" --env-file /tmp/env-vars-for-test-image.list solid-crud
+# docker run --rm --network=testnet --env COOKIE="$COOKIE_ALICE" --env-file test/surface/solid-crud-tests-env.list solid-crud
 docker run --rm --network=testnet --env COOKIE_ALICE="$COOKIE_ALICE" --env COOKIE_BOB="$COOKIE_BOB" --env-file test/surface/web-access-control-tests-env.list web-access-control
 
 docker stop server
