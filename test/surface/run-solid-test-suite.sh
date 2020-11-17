866945f28dcd1c7bacf597bf1901628e193f0826
Michiel de Jong <michiel@unhosted.org>
2020-11-17 12:17:05 +0100

    docker pull testers

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -1,45 +1,50 @@
 #!/bin/bash
 set -e
 
-docker network create testnet
-docker build -t server test/surface/docker/server
-docker build -t cookie test/surface/docker/cookie
+function setup {
+  docker network create testnet
+  docker build -t server test/surface/docker/server
+  docker build -t cookie test/surface/docker/cookie
+  docker pull solidtestsuite/webid-provider-tests:v1.2.0
+  docker pull solidtestsuite/solid-crud-tests:nss-skips
+  docker pull solidtestsuite/web-access-control-tests:latest
+}
+function teardown {
+  docker stop `docker ps --filter network=testnet -q`
+  docker rm `docker ps --filter network=testnet -qa`
+  docker network remove testnet
+}
 
-docker build -t webid-provider test/surface/docker/webid-provider
-docker build -t solid-crud test/surface/docker/solid-crud
-docker build -t web-access-control test/surface/docker/web-access-control
+function startNss {
+  docker run -d --env-file test/surface/$1-env.list --name $1 --network=testnet -v `pwd`:/travis -w /node-solid-server server /travis/bin/solid-test start --config-file /node-solid-server/config.json
+  until docker run --rm --network=testnet solidtestsuite/webid-provider-tests curl -kI https://$1 2> /dev/null > /dev/null
+  do
+    echo Waiting for $1 to start, this can take up to a minute ...
+    docker ps -a
+    docker logs $1
+    sleep 1
+  done
 
-docker run -d --env-file test/surface/server-env.list --name server --network=testnet -v `pwd`:/travis -w /node-solid-server server /travis/bin/solid-test start --config-file /node-solid-server/config.json
-docker run -d --env-file test/surface/thirdparty-env.list --name thirdparty --network=testnet -v `pwd`/test/surface:/surface server /node-solid-server/bin/solid-test start --config-file /surface/thirdparty-config.json
+  docker logs $1
+  echo Getting cookie for $1...
+  export COOKIE_$1="`docker run --cap-add=SYS_ADMIN --network=testnet --env-file ./env-vars-$1.list cookie`"
+}
 
-until docker run --rm --network=testnet webid-provider curl -kI https://server 2> /dev/null > /dev/null
-do
-  echo Waiting for server to start, this can take up to a minute ...
-  docker ps -a
-  docker logs server
-  sleep 1
-done
+function runTests {
+  echo "Running $1 tests against server with cookie $COOKIE_server"
+  docker run --rm --network=testnet \
+    --env COOKIE="$COOKIE_server" \
+    --env COOKIE_ALICE="$COOKIE_server" \
+    --env COOKIE_BOB="$COOKIE_thirdparty" \
+    --env-file test/surface/$1-tests-env.list solidtestsuite/$1-tests
+}
 
-until docker run --rm --network=testnet webid-provider curl -kI https://thirdparty 2> /dev/null > /dev/null
-do
-  echo Server started, waiting for thirdparty to start too ...
-  docker ps -a
-  docker logs thirdparty
-  sleep 1
-done
-
-docker ps -a
-docker logs server
-
-export COOKIE_ALICE="`docker run --rm -e SERVER_ROOT="https://server" --network=testnet cookie`"
-export COOKIE_BOB="`docker run --rm -e SERVER_ROOT="https://thirdparty" --network=testnet cookie`"
-
-docker run --rm --network=testnet --env COOKIE="$COOKIE_ALICE" --env-file test/surface/webid-provider-tests-env.list webid-provider
-docker run --rm --network=testnet --env COOKIE="$COOKIE_ALICE" --env-file test/surface/solid-crud-tests-env.list solid-crud
-docker run --rm --network=testnet --env COOKIE_ALICE="$COOKIE_ALICE" --env COOKIE_BOB="$COOKIE_BOB" --env-file test/surface/web-access-control-tests-env.list web-access-control
-
-docker stop server
-docker rm server
-docker stop thirdparty
-docker rm thirdparty
-docker network remove testnet
+# ...
+teardown || true
+setup
+startNss server
+runTests webid-provider
+# runTests solid-crud
+# startNss thirdparty
+# runTests web-access-control
+teardown
