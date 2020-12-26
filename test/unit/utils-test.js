38544dddbdf5a4c9a2a51d75d024588dc7ba0d7e
Alain Bourgeois <alain.bourgeois10@gmail.com>
2020-12-26 16:17:52 +0100

    adds create container with PUT and more (#1543)
    * delete root returns 405
    
    * delete root/rootAcl throw 405
    
    * no POST for auxiliary resources
    
    * no resource and container with same name
    
    * fake commit
    
    * reverse fake commit
    
    * fake
    
    * reverse fake
    
    * fake
    
    * reverse fake
    
    * delete pod
    
    * cleaning
    
    * cleaning
    
    * some more cleaning
    
    * content-type must
    
    * cleaning
    
    * content-type do not default
    
    * cleaning
    
    * update CRUD checks to solid specifications
    
    * PATCH check filename is not a folder
    
    * cleaning
    
    * pubsub on parent
    
    * add PUT for container
    
    * cleaning

--- test/unit/utils-test.js
+++ test/unit/utils-test.js
@@ -73,8 +73,8 @@ describe('Utility functions', function () {
 
   describe('getContentType()', () => {
     describe('for Express headers', () => {
-      it('should default to application/octet-stream', () => {
-        assert.equal(utils.getContentType({}), 'application/octet-stream')
+      it('should not default', () => {
+        assert.equal(utils.getContentType({}), '')
       })
 
       it('should get a basic content type', () => {
@@ -87,9 +87,9 @@ describe('Utility functions', function () {
     })
 
     describe('for Fetch API headers', () => {
-      it('should default to application/octet-stream', () => {
+      it('should not default', () => {
         // eslint-disable-next-line no-undef
-        assert.equal(utils.getContentType(new Headers({})), 'application/octet-stream')
+        assert.equal(utils.getContentType(new Headers({})), '')
       })
 
       it('should get a basic content type', () => {
