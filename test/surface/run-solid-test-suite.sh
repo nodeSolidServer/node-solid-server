1afe2f45104a87606007fe97ac443462065cc953
Michiel de Jong <michiel@unhosted.org>
2021-02-14 10:45:17 +0100

    solid-crud-tests v3.0.0

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -45,7 +45,7 @@ teardown || true
 setup
 waitForNss server
 runTests webid-provider-tests v2.0.3
-runTests solid-crud-tests v2.1.1
+runTests solid-crud-tests v3.0.0
 waitForNss thirdparty
 runTests web-access-control-tests v2.1.3
 teardown
