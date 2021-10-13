ea1d14cd37143f9a4e9b459e222d450d9cdf83f0
Angelo Veltens <angelo.veltens@online.de>
2021-10-13 19:06:59 +0200

    make sure to run correct NSS version during tests

--- docker-image/test/conftest.py
+++ docker-image/test/conftest.py
@@ -1,11 +1,15 @@
 import docker
 import pytest
+import os
+
 
 @pytest.fixture(scope="session")
 def client():
     return docker.from_env()
 
+
 @pytest.fixture(scope="session")
 def image(client):
-     img, _ = client.images.build(path='./src', dockerfile='Dockerfile')
-     return img
+    img, _ = client.images.build(path='./src', dockerfile='Dockerfile',
+                                 buildargs={"SOLID_SERVER_VERSION": os.environ['SOLID_SERVER_VERSION']})
+    return img
