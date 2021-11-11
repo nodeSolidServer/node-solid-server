33b43dd90225f34a3b537f430289f654ffb10995
Timea <timea.turdean@gmail.com>
2021-11-11 15:09:20 +0100

    New index.html design & added docu links (#1636)
    * new index.html page design and added docu links
    
    * small css change
    
    * Update common/js/index-buttons.js
    
    Co-authored-by: Alain Bourgeois <alain.bourgeois10@gmail.com>
    
    * removed some text and improved code
    
    * Update common/js/index-buttons.js
    
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>
    
    * small css chnages to dd and dt elements
    
    Co-authored-by: Alain Bourgeois <alain.bourgeois10@gmail.com>
    Co-authored-by: Ted Thibodeau Jr <tthibodeau@openlinksw.com>

---
+++ common/js/index-buttons.js
@@ -0,0 +1,41 @@
+'use strict'
+var keyname = 'SolidServerRootRedirectLink';
+function register() { 
+    alert(2); window.location.href = "/register"; 
+}
+document.addEventListener('DOMContentLoaded', async function() {
+    if (!UI.authn.currentUser()) await UI.authn.checkUser();
+    let user = UI.authn.currentUser();
+
+    // IF LOGGED IN: SET SolidServerRootRedirectLink. LOGOUT
+    if( user ) {
+        window.localStorage.setItem(keyname, user.uri);
+        await UI.authn.authSession.logout();
+    }
+    else {
+        let webId = window.localStorage.getItem(keyname);
+
+        // IF NOT LOGGED IN AND COOKIE EXISTS: REMOVE COOKIE, HIDE WELCOME, SHOW LINK TO PROFILE
+        if( webId ) {
+        window.localStorage.removeItem(keyname);
+        document.getElementById('loggedIn').style.display = "block";
+        document.getElementById('loggedIn').innerHTML = `<p>Your WebID is : <a href="${webId}">${webId}</a>.</p> <p>Visit your profile to log into your Pod.</p>`;
+        }
+
+        // IF NOT LOGGED IN AND COOKIE DOES NOT EXIST  
+        //     SHOW WELCOME, SHOW LOGIN BUTTON
+        //     HIDE LOGIN BUTTON, ADD REGISTER BUTTON
+        else {
+        let loginArea = document.getElementById('loginStatusArea');
+        let html = `<input type="button" onclick="window.location.href='/register'" value="Register to get a Pod" class="register-button">`
+        let span = document.createElement("span")
+        span.innerHTML = html
+        loginArea.appendChild(span);
+        loginArea.appendChild(UI.authn.loginStatusBox(document, null, {}))
+        const logInButton = loginArea.querySelectorAll('input')[1];
+        logInButton.value = "Log in to see your WebID";
+        const signUpButton = loginArea.querySelectorAll('input')[2];
+        signUpButton.style.display = "none";
+        }                    
+    }
+})
