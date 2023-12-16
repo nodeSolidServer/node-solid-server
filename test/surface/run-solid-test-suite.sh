c5ccf90e9247ecf553c237a1352055357b4900b4
bourgeoa <alain.bourgeois10@gmail.com>
2023-12-16 21:45:57 +0100

    use test branch

--- test/surface/run-solid-test-suite.sh
+++ test/surface/run-solid-test-suite.sh
@@ -48,7 +48,15 @@ waitForNss server
 runTests webid-provider-tests v2.0.3
 runTests solid-crud-tests v6.0.0
 waitForNss thirdparty
-runTests web-access-control-tests nss-skips # patchAppendNewDocument # v7.1.0
+# runTests web-access-control-tests nss-skips # patchAppendNewDocument # v7.1.0
+docker build -t web-access-control test/surface/docker/web-access-control
+# docker run --rm --network=testnet --env COOKIE_ALICE="$COOKIE_ALICE" --env COOKIE_BOB="$COOKIE_BOB" --env-file test/surface/web-access-control-tests-env.list web-access-control
+echo "Running web-access-control-tests against server with cookie $COOKIE_server"
+docker run --rm --network=testnet \
+  --env COOKIE="$COOKIE_server" \
+  --env COOKIE_ALICE="$COOKIE_server" \
+  --env COOKIE_BOB="$COOKIE_thirdparty" \
+  --env-file test/surface/web-access-control-tests-env.list web-access-control
 teardown
 
 # To debug, e.g. running web-access-control-tests jest interactively,
