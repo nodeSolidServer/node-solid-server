d2495885de000cdcd88bb23e30025966fde2c4d8
Zach <zg009@uark.edu>
2024-03-20 17:47:58 -0500

    wrote tests for n3 patch parser

--- test/unit/n3-patch-parser-test.js
+++ test/unit/n3-patch-parser-test.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const chai = require('chai')
-const fs = require('fs')
-// const expect = chai.expect
+// const fs = require('fs')
+const expect = chai.expect
 // const sinon = require('sinon')
 chai.use(require('sinon-chai'))
 chai.use(require('dirty-chai'))
@@ -10,51 +10,68 @@ chai.should()
 // const HttpMocks = require('node-mocks-http')
 
 const $rdf = require('rdflib')
-// const rewire = require('rewire')
-// const app = rewire('../../lib/handlers/patch/n3-patch-parser.js')
+const rewire = require('rewire')
+const app = rewire('../../lib/handlers/patch/n3-patch-parser.js')
 
-// const queryForFirstResult = app.__get__('queryForFirstResult')
-// const parsePatchDocument = app.__get__('parsePatchDocument')
+const PATCH_NS = 'http://www.w3.org/ns/solid/terms#'
+const PREFIXES = `PREFIX solid: <${PATCH_NS}>\n`
+
+const queryForFirstResult = app.__get__('queryForFirstResult')
+const parsePatchDocument = app.__get__('parsePatchDocument')
 
 describe('n3-patch-parser', () => {
-  const testN3File = './n3-test-doc.ttl'
-  before(() => {
-    const content = 'content'
-    fs.writeFile(testN3File, content, err => {
-      if (err) console.err('failed to write')
-    })
-  })
-
-  after(() => {
-    fs.unlink(testN3File, (err) => {
-      if (err) console.err('failed to delete test file')
-    })
-  })
-
   describe('parsePatchDocument()', () => {
-    it('tests parsePatchDocument()', () => {
-    //   let targetURI // not needed for deprecated
-    //   const patchURI = 'https://example.org'
-    //   const patchGraph = $rdf.graph()
-    //   const patchText = ''
-    //   $rdf.parse(patchText, patchGraph, patchURI, 'text/n3')
+    it('tests parsePatchDocument()', async () => {
+      let targetURI // not needed for deprecated
+      const patchURI = 'http://localhost:7777/resources/read-write.ttl'
+      const patchText = `@prefix solid: <http://www.w3.org/ns/solid/terms#>.
+      @prefix ex: <http://www.example.org/terms#>.
+      
+      _:rename a solid:InsertDeletePatch;
+        solid:deletes { ?a <b> <c>. }.`
+      const result = await parsePatchDocument(targetURI, patchURI, patchText)
+      expect(result).to.not.be.undefined()
+    })
+
+    it('should fail with error 400', () => {
+      let targetURI // not needed for deprecated
+      const patchURI = 'http://localhost:7777/resources/read-write.ttl'
+      const patchText = `@prefix solid: <http://www.w3.org/ns/solid/terms#>.
+      @prefix ex: <http://www.example.org/terms#>.
+      
+      _:rename a solid:InsertDeletePatch;
+        solid:where { ?a <b> <c>. }.`
+
+      parsePatchDocument(targetURI, patchURI, patchText)
+        .catch(err => {
+          expect(err.status).to.be(400)
+          expect(err.message).to.be('Patch should at least contain inserts or deletes.')
+        })
     })
   })
 
   describe('queryForFirstResult()', () => {
     it('tests queryForFirstResult', async () => {
       // let targetURI // not needed for deprecated
-      const patchURI = 'https://example.org'
+      const patchURI = 'http://localhost:7777/resources/read-write.ttl'
       const patchGraph = $rdf.graph()
-      const patchText = ''
+      const patchText = `@prefix solid: <http://www.w3.org/ns/solid/terms#>.
+      @prefix ex: <http://www.example.org/terms#>.
+      
+      _:rename a solid:InsertDeletePatch;
+        solid:deletes { ?a <b> <c>. }.`
       $rdf.parse(patchText, patchGraph, patchURI, 'text/n3')
-      // const result = await queryForFirstResult(patchGraph, `${PREFIXES}
-      // SELECT ?insert ?delete ?where WHERE {
-      //   ?patch solid:patches <${targetURI}>.
-      //   OPTIONAL { ?patch solid:inserts ?insert. }
-      //   OPTIONAL { ?patch solid:deletes ?delete. }
-      //   OPTIONAL { ?patch solid:where   ?where.  }
-      // }`)
+      const result = await queryForFirstResult(patchGraph, `${PREFIXES}
+      SELECT ?insert ?delete ?where WHERE {
+        ?patch a solid:InsertDeletePatch.
+        OPTIONAL { ?patch solid:inserts ?insert. }
+        OPTIONAL { ?patch solid:deletes ?delete. }
+        OPTIONAL { ?patch solid:where   ?where.  }
+      }`)
+      const { '?insert': insert, '?delete': deleted, '?where': where } = result
+      expect(insert).to.be.undefined()
+      expect(deleted).to.not.be.undefined()
+      expect(where).to.be.undefined()
     })
   })
 })
